6914f3ef13caaf1d3e87604f98be6e2b4d014974
DP3PO <williampferebee@gmail.com>
2021-04-01 16:25:37 -0400

    still havnt played around with this just needed to fix a slightly urgent issue

--- tweet.js
+++ tweet.js
@@ -11,6 +11,63 @@ const client = new Twitter({
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 })
 
+
+const myArray = []
+
+const catPicFunction = () => {
+
+
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://mlemapi.p.rapidapi.com/randommlem?orientation=landscape",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": process.env.X_Key,
+		"x-rapidapi-host": process.env.X_Host
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+})
+
+
+        // After the data from the AJAX request comes back
+        .then(function(response) {
+          
+
+   
+          if(myArray.includes(response.id)){
+            catPicFunction();
+          } else {
+            myArray.push(response.id);
+          
+          // Saving the image_original_url property
+          var imageUrl = response.url;
+
+          // Creating and storing an image tag
+          var catImage = $("<img>");
+
+          // Setting the catImage src attribute to imageUrl
+          catImage.attr("src", imageUrl);
+          catImage.attr("alt", "cat image");
+          catImage.attr("height", "335");
+          catImage.attr("width", "600");
+          // Prepending the catImage to the images div
+          $("#images").prepend(catImage);
+          }
+        });
+      
+    };
+
+
+    $("#cat-button").on("click", catPicFunction);
+
+
+
+
+
 const imageData = fs.readFileSync("./media/george.jpg") //replace with the path to your image
 
 client.post("media/upload", {media: imageData}, function(error, media, response) {
